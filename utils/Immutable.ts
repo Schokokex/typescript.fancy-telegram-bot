362e6372c5fdf124a206dd12021ff85a2017f9f8
JJ <git@jj22.de>
2020-12-09 01:42:28 +0000

    documented a bug

--- utils/Immutable.ts
+++ utils/Immutable.ts
@@ -4,6 +4,15 @@
  * but i guess its not hard enough against typecasting
  * 
  * 
+ * 
+ * fails to be compatible to "Date"-Objects
+ * 
+ * const b: Immutable<Date> = new Date(1);
+ * const asd: Date = b; // Error
+ * 
+ * 
+ * 
+ * 
  * credits and thanks to https://templecoding.com/blog/real-immutable-types-with-typescript
  */
 export type Immutable<T> = {
