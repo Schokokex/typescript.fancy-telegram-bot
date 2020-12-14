a2451220dc95b8ba2509c1d7eaccfbca809f673a
JJ <git@jj22.de>
2020-12-14 21:12:12 +0000

    added immutability

--- utils/DefinedArray.ts
+++ utils/DefinedArray.ts
@@ -3,5 +3,6 @@ export default class DefinedArray<T> extends Array<T>{
     constructor(...values: (T | undefined)[]){
         const asd = <T[]>values.filter(v=>v!==undefined);
         super(...asd);
+        Object.freeze(this);
     }
 }
