5c94a55b6a12cf85929b3ea7d65908aa08fbd29c
JJ <git@jj22.de>
2020-12-15 20:18:20 +0000

    found a workaround for Immutable bug

--- utils/Immutable.ts
+++ utils/Immutable.ts
@@ -3,20 +3,18 @@
  * but i guess its not hard enough against typecasting
  *
  *
- *
+ * credits and thanks to https://templecoding.com/blog/real-immutable-types-with-typescript
+ * --> original
  * fails to be compatible to "Date"-Objects
  *
  * const b: Immutable<Date> = new Date(1);
  * const asd: Date = b; // Error
  *
+ * --> workaround
+ * adding "T &" {...
  *
- *
- *
- * credits and thanks to https://templecoding.com/blog/real-immutable-types-with-typescript
  *
  * @format
  */
 
-export type Immutable<T> = {
-	readonly [K in keyof T]: Immutable<T[K]>;
-};
+export type Immutable<T> = T & { readonly [K in keyof T]: Immutable<T[K]> };
